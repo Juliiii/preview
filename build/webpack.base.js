5fd82b12c1e30cba7fbabba69312279566f954af
Juliiii <zhulf6@mail2.sysu.edu.cn>
2017-09-23 11:51:25 +0800

    配置环境

---
+++ build/webpack.base.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CleanWepackPlugin = require('clean-webpack-plugin');
+const HtmlWepackPlugin = require('html-webpack-plugin');
+
+function resolve (url) {
+  return path.join(__dirname, url);
+}
+
+module.exports = {
+  entry: resolve('../src/index.js'),
+  output: {
+    filename: 'bundle.[hash].js',
+    path: resolve('../dist')
+  },
+  resolve: {
+    extensions: ['.js', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          { loader: 'postcss-loader', options: require('./postcss.config') }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWepackPlugin(['dist']),
+    new HtmlWepackPlugin(),
+  ]
+};
