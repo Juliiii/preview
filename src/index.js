50949fc2aec44d6b344ff0fc258974a3a394bfc6
ZhuLifeng <820917549@qq.com>
2017-09-24 02:27:28 +0800

    start core

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import './index.css';
 let father;
 let instance;
 let close;
+let ratio;
 let imgs;
 let current;
 let total;
 let clientWidth;
+let startX;
+let endX;
 let defaultSrcs = ['http://wx1.sinaimg.cn/mw690/005JtfT5ly1fjswr0d89kj32ao328qv6.jpg', 'http://wx1.sinaimg.cn/mw690/005JtfT5ly1fjswr6uy2rj32ao328b2g.jpg'];
+let speed = 40;
 
 function preview (srcs = defaultSrcs) {
   // 初始化
@@ -17,7 +21,7 @@ function preview (srcs = defaultSrcs) {
   elem.setAttribute('class', 'preview-wrapper');
 
   // 右上角的当前图片 / 所有图片
-  let ratio = document.createElement('div');
+  ratio = document.createElement('div');
   ratio.setAttribute('class', 'ratio');
   ratio.innerText = `${current}/${total}`;
 
@@ -42,7 +46,7 @@ function createImgs (srcs) {
   let wrapper = document.createElement('div');
   wrapper.setAttribute('class', 'preview-innerWrapper');
   wrapper.style.width = `${clientWidth * srcs.length}px`;
-  let imgs = document.createDocumentFragment();
+  let list = document.createDocumentFragment();
   const { length } = srcs;
   for (let i = 0; i < length; i++) {
     // 创建图片标签和图片的wrapper的标签
@@ -56,22 +60,60 @@ function createImgs (srcs) {
     img.setAttribute('class', 'img');
     img.setAttribute('src', srcs[i]);
     div.appendChild(img);
-    imgs.appendChild(div);
-    imgs
+    list.appendChild(div);
+    imgs.push(div);
   }
-  wrapper.appendChild(imgs);
+  wrapper.appendChild(list);
   instance.appendChild(wrapper);
 }
 
-function slide () {
+function slide (dir) {
+  if (dir === 'pre') {
+    animate(current, 0 - speed, 0);
+    animate(current - 1, 0 - speed, clientWidth);
+    if (current !== 1) {
+      for (let i = 0; i < current - 1; i++) {
+        imgs[i].style.left = `${(i + 1 - current) * clientWidth}px`;
+      }
+    }
+    for (i = current + 1; i < imgs.length; i++) {
+      imgs[i].style.left = `${(i + 1 - current) * clientWidth}px`;
+    }
+  } else {
+    animate(current, speed, 0);
+    animate(current - 1, speed, 0 - clientWidth);
+    if (current !== 1) {
+      for (let i = 0; i < current - 1; i++) {
+        imgs[i].style.left = `${(i + 1 - current) * clientWidth}px`;
+      }
+    }
+    for (i = current + 1; i < imgs.length; i++) {
+      imgs[i].style.left = `${(i + 1 - current) * clientWidth}px`;
+    }    
+  }
+}
 
+function animate (index ,offset, boundary) {
+  const elem = imgs[index - 1];
+  let timeId = setInterval(() => {
+    const left = parseInt(elem.style.left);
+    if (left + offset >= boundary) {
+      elem.style.left = `${boundary}px`;
+      clearInterval(timeId);
+    } else {
+      elem.style.left = `${left + offset}px`;
+    }
+  }, 10);  
 }
 
 function init (srcs) {
   instance = null;
   instance = null;
   close = null;
-  imgs = null;
+  ratio = null;
+  startX = null;
+  endX = null;
+  imgs = [];
   current = 1;
   total = srcs.length;
   clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
@@ -87,17 +129,29 @@ function destory (e) {
 
 function bindEvent () {
   instance.addEventListener('click', destory);
-  instance.addEventListener('touchstart', () => console.log(1));
-  instance.addEventListener('touchmove', (...args) => console.log(args));
-  instance.addEventListener('touchend', (...args) => console.log(args));
+  instance.addEventListener('touchstart', (e) => {
+    startX = e.targetTouches[0].pageX;
+  });
+  instance.addEventListener('touchmove', (e) => {
+    endX = e.targetTouches[0].pageX;
+  });
+  instance.addEventListener('touchend', (e) => {
+    if (endX > startX && endX > startX + 20) {
+      if (current === 1) return;
+      current--;
+      slide('pre');
+    } else if (endX < startX && endX < startX + 20) {
+      if (current === imgs.length) return;
+      current++;
+      slide('next');
+    }
+    console.log(current)
+    ratio.innerText = `${current}/${total}`;
+  });
   close.addEventListener('click', destory);
-  instance.ontouchmove = () => {
-    console.log('move');
-  }
-  instance.ontouchend = () => {
-    console.log('end');
-  }
 }
+
+
 
 
 preview();
